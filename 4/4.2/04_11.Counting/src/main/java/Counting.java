fc4978218ad1615eb1dab29fd0a5327357892b50
Arvydas Skripka <arvydas.skripka45@gmail.com>
2025-01-08 19:13:13 +0200

    04_11 task is completed

---
+++ 4/4.2/04_11.Counting/src/main/java/Counting.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class Counting {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = Integer.parseInt(scanner.nextLine()); // skaicius kuri useris iterps
+        for (int i = 0; i <= a; i++) {
+            System.out.println(i);
+        }
+    }
+}
