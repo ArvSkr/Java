ee07f8ca33cbe2440fc35608330cef17c3e36011
Arvydas Skripka <arvydas.skripka45@gmail.com>
2025-01-08 19:26:16 +0200

    04_13  task is completed

--- 4/4.2/04_13.FromWhereToWhere/src/main/java/FromWhereToWhere.java
+++ 4/4.2/04_13.FromWhereToWhere/src/main/java/FromWhereToWhere.java
@@ -5,7 +5,12 @@ public class FromWhereToWhere {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // Write your program here
+        System.out.print("Where to? ");
+        int a = Integer.parseInt(scanner.nextLine()); // skaicius kuri useris iterps
+        System.out.print("Where from ? ");
+        int b = Integer.parseInt(scanner.nextLine());
+        for (int i = b; i <= a; i++) {
+            System.out.println(i);
+        }
     }
 }
