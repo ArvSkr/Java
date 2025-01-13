a5c4dcd318a3d56f9464066ef493600fe4a4c6ba
Arvydas Skripka <arvydas.skripka45@gmail.com>
2025-01-13 19:02:26 +0200

    ArraysTask done

--- 6/6.3/06_3_arrays/src/main/java/lt/techin/praktinis/ArraysTask.java
+++ 6/6.3/06_3_arrays/src/main/java/lt/techin/praktinis/ArraysTask.java
@@ -10,22 +10,25 @@ public class ArraysTask {
         // Pateiktas masyvas ir pavyzdys programos patikrinimui. Masyvo elementų reikšmes galite keisti.
 
         int[] arr = {3, 2, 1, 4, 6, 5};
-
-     int[] arrWithRemovedElement = removeElementAndReturnNewArray(arr, 1);
-       System.out.println(Arrays.toString(arrWithRemovedElement));
+        int[] generator = generateRandomArray(4,2,10);
+        int[] removerElement = removeElementAndReturnNewArray(arr, 10);
+        int [] Elementinserter = insertElementAndReturnNewArray(arr,4,6);
+        int[] reverser = reverseArray(arr);
+        int[] reverserinplace = reverseInPlace(arr);
+        int[] arrWithRemovedElement = removeElementAndReturnNewArray(arr, 1);
+        System.out.println(Arrays.toString(arrWithRemovedElement));
 
     }
 
     // Parašykite metodą, kuris sukuria nustatyto dydžio sveikųjų skaičių masyvą
     // ir užpildo jį atsitiktinai sugeneruotais skaičiais iš nurodyto intervalo.
     public static int[] generateRandomArray(int size, int min, int max) {
-
-            int[] array = new int[size];
-            for (int i = 0; i < size; i++) {
-                array[i] = (int) (Math.random() * (max - min + 1)) + min;
-            }
-            return array;
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = (int) (Math.random() * (max - min + 1)) + min;
         }
+        return array;
+    }
     }
 
 
@@ -41,28 +44,45 @@ public class ArraysTask {
             }
         }
         return newArr;
+        return null;
     }
 
     //Parašykite metodą, kuris įterptų į masyvą arr[n] prieš k-tąjį elementą reikšmę x,
     //surašant reikiamus elementus į naują masyvą b[n+1]. Metodas grąžina naują masyvą.
     //Jei k yra už masyvo ribų, metodas grąžina null (teisingiau būtų mesti exception, bet dar nesimokėm)
     public static int[] insertElementAndReturnNewArray(int[] arr, int k, int x) {
-        //TODO
-        return null;
+        if (k < 0 || k > arr.length) return null;
+        int[] newArr = new int[arr.length + 1];
+        for (int i = 0, j = 0; i < newArr.length; i++) {
+            if (i == k) {
+                newArr[i] = x;
+            } else {
+                newArr[i] = arr[j++];
+            }
+        }
+        return newArr;
+
     }
 
     //Parašykite metodą, kuris grąžintų duoto masyvo apverstą kopija.
     public static int[] reverseArray(int[] arr) {
-        //TODO
+        int[] reversed = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            reversed[i] = arr[arr.length - 1 - i];
+        }
+        return reversed;
         return null;
     }
 
     //Parašykite metodą, kuris apverstu duotą masyvą (nekuriant naujo masyvo)
     public static void reverseInPlace(int[] arr) {
-        //TODO
+        for (int i = 0; i < arr.length / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[arr.length - 1 - i];
+            arr[arr.length - 1 - i] = temp;
+        }
 
     }
 
 
 }
-
