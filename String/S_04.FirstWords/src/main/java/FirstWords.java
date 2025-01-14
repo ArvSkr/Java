201a979ac2207b01612d2470a5e9b82dfc059afd
Arvydas Skripka <arvydas.skripka45@gmail.com>
2025-01-14 18:36:46 +0200

    done

--- String/S_04.FirstWords/src/main/java/FirstWords.java
+++ String/S_04.FirstWords/src/main/java/FirstWords.java
@@ -5,7 +5,23 @@ public class FirstWords {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        while (true) {
 
+            String text = scanner.nextLine();
+
+
+            if (text.isEmpty()) {
+                break;
+            }
+
+
+            String[] parts = text.split("\\s+");
+
+
+            if (parts.length > 0) {
+                System.out.println(parts[0]);
+            }
+        }
 
     }
 }
