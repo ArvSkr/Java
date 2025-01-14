f134cfb22224d884d662ef9b24378a320edc5b88
Arvydas Skripka <arvydas.skripka45@gmail.com>
2025-01-14 19:54:34 +0200

    done

--- String/S_08.PersonalDetails/src/main/java/PersonalDetails.java
+++ String/S_08.PersonalDetails/src/main/java/PersonalDetails.java
@@ -8,5 +8,52 @@ public class PersonalDetails {
         Scanner scanner = new Scanner(System.in);
 
 
+        String longestName = "";
+        int longestNameLength = 0;
+        ArrayList<Integer> birthYears = new ArrayList<>();
+        int count = 0;
+        
+
+        while (true) {
+            String input = scanner.nextLine();
+
+
+            if (input.isEmpty()) {
+                break;
+            }
+
+
+            String[] parts = input.split(",");
+
+
+            if (parts.length == 2) {
+                String name = parts[0];
+                int birthYear = Integer.parseInt(parts[1]);
+
+
+                if (name.length() > longestNameLength) {
+                    longestNameLength = name.length();
+                    longestName = name;
+                }
+
+
+                birthYears.add(birthYear);
+                count++;
+            }
+        }
+
+
+        if (count > 0) {
+
+            double totalBirthYear = 0;
+            for (int year : birthYears) {
+                totalBirthYear += year;
+            }
+            double averageBirthYear = totalBirthYear / count;
+
+
+            System.out.println("Longest name: " + longestName);
+            System.out.println("Average birth year: " + averageBirthYear);
+        }
     }
 }
