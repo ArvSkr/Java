3a991bfd05935ec37d16415b6c4bef023464cf42
Arvydas Skripka <arvydas.skripka45@gmail.com>
2025-01-14 18:30:18 +0200

    done

--- String/S_03.AVClub/src/main/java/AVClub.java
+++ String/S_03.AVClub/src/main/java/AVClub.java
@@ -5,7 +5,21 @@ public class AVClub {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String text = scanner.nextLine();
 
+            
+            if (text.isEmpty()) {
+                break;
+            }
+
+            String[] space = text.split(" ");
+            for (int i = 0; i < space.length; i++) {
+                if (space[i].contains("av")) {
+                    System.out.println(space[i]);
+                }
+            }
+        }
 
     }
 }
