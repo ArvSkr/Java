356097f9c2baa60191f77dbb6f2a15d2277d7ca6
Arvydas Skripka <arvydas.skripka45@gmail.com>
2025-01-14 19:24:02 +0200

    done

--- String/S_05.LastWords/src/main/java/LastWords.java
+++ String/S_05.LastWords/src/main/java/LastWords.java
@@ -5,7 +5,21 @@ public class LastWords {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        while (true) {
 
+            String input = scanner.nextLine();
+
+
+            if (input.isEmpty()) {
+                break;
+            }
+
+
+            String[] parts = input.split("\\s+");
+
+
+            System.out.println(parts[parts.length - 1]);
+        }
 
     }
 }
