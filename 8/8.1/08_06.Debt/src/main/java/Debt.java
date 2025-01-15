0a0737dab6791fe558e319b567a822dc50850471
Arvydas Skripka <arvydas.skripka45@gmail.com>
2025-01-15 12:18:28 +0200

    08_06 task complete

---
+++ 8/8.1/08_06.Debt/src/main/java/Debt.java
@@ -0,0 +1,20 @@
+import java.net.SocketTimeoutException;
+
+public class Debt {
+    private double balance;
+    private double interestRate;
+
+    public Debt(double initialBalance, double initialInterestRate) {
+        balance = initialBalance;
+        interestRate = initialInterestRate;
+    }
+
+    public void printBalance() {
+        System.out.println(balance);
+    }
+
+    public void waitOneYear() {
+        this.balance *= this.interestRate;
+    }
+
+}
