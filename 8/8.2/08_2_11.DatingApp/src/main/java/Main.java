45c577fba375c8ae109dcac9d031a1c960b94aaa
Arvydas Skripka <arvydas.skripka45@gmail.com>
2025-01-16 17:07:42 +0200

    done

--- 8/8.2/08_2_11.DatingApp/src/main/java/Main.java
+++ 8/8.2/08_2_11.DatingApp/src/main/java/Main.java
@@ -2,14 +2,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        SimpleDate date = new SimpleDate(29,12,2011);
 
-        int week = 1;
-        while(week <= 7){
-            System.out.println("Friday after " + week + " week is " + date);
-            date = date.afterNumberOfdays(7);
-            week++;
-        }
 
     }
 }
--- 8/8.2/08_2_11.DatingApp/src/main/java/SimpleDate.java
+++ 8/8.2/08_2_11.DatingApp/src/main/java/SimpleDate.java
@@ -1,4 +1,3 @@
-
 public class SimpleDate {
 
     private int day;
@@ -6,6 +5,9 @@ public class SimpleDate {
     private int year;
 
     public SimpleDate(int day, int month, int year) {
+        if (day < 1 || day > 30 || month < 1 || month > 12) {
+            throw new IllegalArgumentException("Invalid date");
+        }
         this.day = day;
         this.month = month;
         this.year = year;
@@ -26,58 +28,42 @@ public class SimpleDate {
         }
 
         if (this.year == compared.year && this.month == compared.month &&
-                 this.day < compared.day) {
+                this.day < compared.day) {
             return true;
         }
 
         return false;
     }
-    public void advance(){
-        if (day < 30 && day != 30){
-            day++;
-        }
-        if(day == 30 && month < 12 ){
-            day -= 30;
-            month++;
-        } else if(month == 12 && day == 30){
-            month -= 11;
-            year++;
-        }
 
-
+    public void advance() {
+        this.advance(1);
     }
-    public void advance(int howManyDays){
 
-        if (day < 30){
-            day += howManyDays;
-        }
-        if(day > 30 && month < 12 ){
-            day -= 29;
-            month++;
-        }
-        if(month == 12 && day == 30){
-            month -= 11;
-            year++;
-            day -= (30 - howManyDays);
+    public void advance(int howManyDays) {
+        if (howManyDays < 0) {
+            throw new IllegalArgumentException("Number of days to advance must be non-negative");
         }
 
+        this.day += howManyDays;
+
+        while (this.day > 30) {
+            this.day -= 30;
+            this.month++;
+
+            if (this.month > 12) {
+                this.month = 1;
+                this.year++;
+            }
+        }
     }
-    public SimpleDate afterNumberOfdays(int days)
-    {   SimpleDate newDate = new SimpleDate(day, month, year);
-        if (day < 30) {
-            day += days;
-        }
-        if (day > 30 && month < 12) {
-            day -= 29;
-            month++;
-        }
-        if (month == 12 && day == 30) {
-            month -= 11;
-            year++;
-            day -= (30 - days);
+
+    public SimpleDate afterNumberOfDays(int days) {
+        if (days < 0) {
+            throw new IllegalArgumentException("Number of days must be non-negative");
         }
 
-
+        SimpleDate newDate = new SimpleDate(this.day, this.month, this.year);
+        newDate.advance(days);
         return newDate;
     }
 }
