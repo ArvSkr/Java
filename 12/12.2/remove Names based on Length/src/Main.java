906f8654e8ced920514fcfb599492accd964e1e8
Arvydas Skripka <arvydas.skripka45@gmail.com>
2025-01-22 14:34:01 +0200

    remove names vased on length DONE

---
+++ 12/12.2/remove Names based on Length/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>(Arrays.asList("Tom", "Lisa", "Eve", "Robert", "Kate"));
+        Supplier<String> nameremover = () -> {
+            String result = "";
+            for (String name : names) {
+                result += name + "\n";
+            }
+
+            names.removeIf(name -> name.length() <= 3);
+            return result;
+        };
+
+        System.out.println(nameremover.get());
+
+        names.forEach(System.out::println);
+    }
+}
