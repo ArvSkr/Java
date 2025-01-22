f3aa0baf7ff3441dae7b0eed30afeff1d190e7ea
Arvydas Skripka <arvydas.skripka45@gmail.com>
2025-01-22 14:44:45 +0200

    Modify names task done

---
+++ 12/12.2/Modify Names/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>(Arrays.asList("Alice", "Bob", "Charlie"));
+        Supplier<String> namereplacer = () -> {
+            String result = "";
+            for (String name : names) {
+                result += name + "\n";
+            }
+
+            names.replaceAll(String::toUpperCase);
+            return result;
+        };
+
+        System.out.println(namereplacer.get());
+
+        names.forEach(System.out::println);
+    }
+}
