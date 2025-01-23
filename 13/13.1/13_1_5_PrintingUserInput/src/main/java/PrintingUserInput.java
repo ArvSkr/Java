44891c3095c5f2e9c5c7a6e55e789c230dbc5b5e
Arvydas Skripka <arvydas.skripka45@gmail.com>
2025-01-23 11:38:37 +0200

    task done

---
+++ 13/13.1/13_1_5_PrintingUserInput/src/main/java/PrintingUserInput.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class PrintingUserInput {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<String> words = new ArrayList<>();
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.isEmpty()) {
+                break;
+            }
+            words.add(input);
+
+        }
+        words.stream()
+                .forEach(System.out::println);
+    }
+}
