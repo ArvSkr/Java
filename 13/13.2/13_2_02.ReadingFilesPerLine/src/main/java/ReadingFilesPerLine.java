0806f0e4711d462c125ca66802f9b85522842c89
Arvydas Skripka <arvydas.skripka45@gmail.com>
2025-01-23 17:32:09 +0200

    2 task done

--- 13/13.2/13_2_02.ReadingFilesPerLine/src/main/java/ReadingFilesPerLine.java
+++ 13/13.2/13_2_02.ReadingFilesPerLine/src/main/java/ReadingFilesPerLine.java
@@ -1,17 +1,28 @@
-
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ReadingFilesPerLine {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        // test the method here
+        System.out.println("Enter the file path:");
+        String filePath = scanner.nextLine();
 
+        List<String> lines = read(filePath);
+        for (String line : lines) {
+            System.out.println(line);
+        }
     }
 
+    public static List<String> read(String filePath) throws Exception {
+        List<String> lines = new ArrayList<>();
+        Stream<String> stream = Files.lines(Paths.get(filePath));
+        lines = stream.collect(Collectors.toList());
+        return lines;
+    }
 }
