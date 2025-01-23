81dad5b5419ed14e6783374a6f91945b97acaae4
Arvydas Skripka <arvydas.skripka45@gmail.com>
2025-01-23 11:07:26 +0200

    task done

--- 13/13.1/13_1_1_AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ 13/13.1/13_1_1_AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -3,9 +3,10 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
-public class PositiveNumbers {
+public class AverageOfNumbers {
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         List<Integer> numbers = new ArrayList<>();
         System.out.println("Input numbers, type \"end\" to stop");
@@ -19,13 +20,14 @@ public class PositiveNumbers {
             numbers.add(number);
         }
 
-        List<Integer> positiveNumbers = positive(numbers);
-        
+
+        double average = numbers.stream()
+                .mapToDouble(Integer::doubleValue)
+                .average()
+                .orElse(0.0);
+
+        System.out.println("Average of positive numbers: " + average);
     }
 
-    public static List<Integer> positive(List<Integer> numbers) {
-        return numbers.stream()
-                .filter(number -> number > 0)
-                .collect(Collectors.toList());
-    }
+
 }
